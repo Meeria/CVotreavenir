79d2415d0d7b363dec30121430da3a4501ff370d
Meeria <meria.dap@gmail.com>
2023-04-27 22:56:40 +0200

    post linkedin

--- views/v_post.php
+++ views/v_post.php
@@ -2,32 +2,32 @@
 include("inc/header.php");
 ?>
 
-<h1 class="title-page-starter">CREATION D'UN<span class="pink-words"> POST LINKEDIN</span></h1>
+<h1 class="title-page-post">CREATION D'UN<span class="pink-words"> POST LINKEDIN</span></h1>
 
 <div class="container-starter">
     <div class="img-starter">
         <img src="../public/illustrations_site/post.png" alt="">
     </div>
     <div class="text-starter">
-        <h2>Un CV professionnel pour vous démarquer des autres candidats.</h2>
+        <h2>LinkedIn est le premier réseau social professionnel, être présent(e) est <span class="pink-words">incontournable</span>.</h2>
         <div class="line-starter">
             <img src="../public/illustrations_site/pink-check.png" alt="">
-            <p><span class="pink-words">Rédaction de votre CV</span>, après vos réponses au questionnaire ou un entretien téléphonique d’environ 20/30 minutes.</p>
+            <p><span class="pink-words">Rédaction du post pour votre recherche d'emploi</span> 97% des recruteurs utilisent Linkedin</p>
         </div>
         <div class="line-starter">
             <img src="../public/illustrations_site/pink-check.png" alt="">
-            <p><span class="pink-words">Mise en forme du CV</span> selon les standards de votre secteur pour attirer l’attention des recruteurs</p>
+            <p><span class="pink-words">Création sous 24h</span> par nos équipes  </p>
         </div>
         <div class="line-starter">
             <img src="../public/illustrations_site/pink-check.png" alt="">
-            <p><span class="pink-words">Optimisation des mots-clés</span> pour que le CV remonte dans les résultats des CVthèques et soit compatible avec les ATS (Logiciels RH)</p>
+            <p><span class="pink-words">Optimisation des mots-clés</span> pour que votre post devienne viral, afin d'attirer l'attention des recruteurs</p>
         </div>
         <div class="line-starter">
             <img src="../public/illustrations_site/pink-check.png" alt="">
-            <p><span class="pink-words">Livraison du CV au format Word et PDF</span>. Possibilité de faire des allers-retours avec votre consultant jusqu’à ce que vous validiez la prestation.</p>
+            <p>Possibilité de faire des allers-retours avec votre consultant jusqu’à ce que vous validiez la prestation.</p>
         </div>
     </div>
 </div>
-<div class="btn-price-starter"><a href="<?= \router\url('services','afficher') ?>"><button>32,99€</button></a></div>
+<div class="btn-price-starter"><a href="<?= \router\url('services','afficher') ?>"><button>4,99€</button></a></div>
 
 <?php include("inc/footer.php"); ?>
